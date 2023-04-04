95229fa9ca0876e13ce670d3c11afb4733eea8dd
AlexKV <bloodklesh@gmail.com>
2023-04-05 02:33:43 +0300

    code update

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -11,37 +11,28 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-
     public static String generateDate(int shift) {
         LocalDate date = LocalDate.now().plusDays(shift);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return date.format(formatter);
     }
 
-
-
-    public static class CityGenerator {
-        public static String generateCity(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            String city = faker.address().city();
-            return city;
-        }
+    public static String generateCity(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String city = faker.address().city();
+        return city;
     }
 
-    public static class NameGenerator {
-        public static String generateName(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            String name = faker.name().fullName();
-            return name;
-        }
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().fullName();
+        return name;
     }
 
-    public static class PhoneGenerator {
-        public static String generatePhone(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            String phone = faker.phoneNumber().phoneNumber();
-            return phone;
-        }
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
     }
 
     public static class Registration {
@@ -49,9 +40,9 @@ public class DataGenerator {
         }
 
         public static UserInfo generateUser(String locale) {
-            String city = CityGenerator.generateCity(locale);
-            String name = NameGenerator.generateName(locale);
-            String phone = PhoneGenerator.generatePhone(locale);
+            String city = generateCity(locale);
+            String name = new DataGenerator().generateName(locale);
+            String phone = new DataGenerator().generatePhone(locale);
             return new UserInfo(city, name, phone);
         }
     }
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -4,14 +4,16 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.openqa.selenium.Keys.BACK_SPACE;
+
 
 class DeliveryTest {
 
@@ -28,38 +30,34 @@ class DeliveryTest {
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        String city = DataGenerator.CityGenerator.generateCity("ru");
-        String name = DataGenerator.NameGenerator.generateName("ru");
-        String phone = DataGenerator.PhoneGenerator.generatePhone("ru");
+        String city = DataGenerator.generateCity("ru");
+        String name = DataGenerator.generateName("ru");
+        String phone = DataGenerator.generatePhone("ru");
 
         Configuration.holdBrowserOpen = true;
         $("span[data-test-id = city]").click();
         $("input[placeholder=Город]").setValue(city);
         $("span[class=menu-item__control]").click();
-        $x("//input[@placeholder='Дата встречи']").sendKeys(BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $x("//input[@placeholder='Дата встречи']").setValue(firstMeetingDate);
-        $x("//td[@class='calendar__day calendar__day_state_current']").click();
-        $("span[data-test-id=name]").click();
         $("input[name=name]").setValue(name);
         $("span[data-test-id=phone]").click();
         $("input[name=phone]").setValue(phone);
         $("label[data-test-id=agreement]").click();
-        $x("//button[contains(@class, 'button_view_extra') and contains(@class, 'button_size_m') and contains(@class, 'button_theme_alfa-on-white') and span/span[text()='Запланировать']]").click();
+        $(byText("Запланировать")).click();
         $(withText("Успешно!")).should(appear, Duration.ofSeconds(11));
         $x("//div[contains(text(), 'Успешно!')]");
         refresh();
         $("span[data-test-id = city]").click();
         $("input[placeholder=Город]").setValue(city);
         $("span[class=menu-item__control]").click();
-        $x("//input[@placeholder='Дата встречи']").sendKeys(BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE, BACK_SPACE);
+        $x("//input[@placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $x("//input[@placeholder='Дата встречи']").setValue(secondMeetingDate);
-        $x("//td[@class='calendar__day calendar__day_state_current']").click();
-        $("span[data-test-id=name]").click();
         $("input[name=name]").setValue(name);
         $("span[data-test-id=phone]").click();
         $("input[name=phone]").setValue(phone);
         $("label[data-test-id=agreement]").click();
-        $x("//button[contains(@class, 'button_view_extra') and contains(@class, 'button_size_m') and contains(@class, 'button_theme_alfa-on-white') and span/span[text()='Запланировать']]").click();
+        $(byText("Запланировать")).click();
         $x("//*[contains(text(), 'У вас уже запланирована встреча на другую дату. Перепланировать?!");
         $x("//span[contains(text(), 'Перепланировать')]").click();
         $x("//div[contains(text(), 'Успешно!')]");
